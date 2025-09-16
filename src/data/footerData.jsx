9825604a5489fc39ca8816c27b29e8c5919a73d0
Florent Atcheakou <florent.atcheakou@univ-toulouse.fr>
2025-09-16 10:12:38 +0200

    template

---
+++ src/data/footerData.jsx
@@ -0,0 +1,57 @@
+const footerData = {
+    brand: {
+        name: "Hizkia Siahaan",
+        icon: "bx bx-code-alt",
+        description:
+            "Frontend Developer specializing in UI/UX, modern web technologies, and building smooth digital experiences with clean code & creative flow.",
+    },
+
+    navigationIcon: "bx bx-navigation",
+    socialsIcon: "bx bx-share-alt",
+
+    navigation: [
+        { label: "Home", href: "#home" },
+        { label: "About", href: "#about" },
+        { label: "Projects", href: "#projects" },
+        { label: "Contact", href: "#contact" },
+    ],
+
+    legalLinks: [
+        { label: "Privacy Policy", href: "#privacy" },
+        { label: "Terms of Use", href: "#terms" },
+        { label: "Sitemap", href: "#sitemap" },
+    ],
+
+
+    socials: [
+        {
+            label: "WhatsApp",
+            icon: "bx bxl-whatsapp",
+            href: "https://wa.me/62XXXXXXXXXX"
+        },
+        {
+            label: "Instagram",
+            icon: "bx bxl-instagram",
+            href: "https://www.instagram.com/username"
+        },
+        {
+            label: "GitHub",
+            icon: "bx bxl-github",
+            href: "https://github.com/Hizkia178"
+        }
+    ],
+
+
+
+
+    formSubscription: {
+        title: "Subscribe to Newsletter",
+        description: "Stay updated with my latest projects and articles.",
+        placeholder: "Enter your email",
+        buttonText: "Subscribe",
+    },
+
+    copyright: "© 2025 Hizkia Siahaan. All rights reserved.",
+};
+
+export default footerData;
